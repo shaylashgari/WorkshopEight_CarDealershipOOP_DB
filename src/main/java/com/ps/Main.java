e41ffd1151c0dfc9456b6c52dc4509fc908849fd
Shay <shaylashgari@gmail.com>
2024-12-03 13:46:11 -0600

    latest

---
+++ src/main/java/com/ps/Main.java
@@ -0,0 +1,23 @@
+package com.ps;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create schema for Vehicles and Dealerships, SQL
+        // Create classes (OOP) for vehicles and Dealerships
+        // Create CLI user Interface (Vehicle CRUD)-> (Create, Read, Update, Delete)
+
+        // Manage Vehicles
+            // - Get a vehicle by ID
+            // - Get all vehicles
+            // - Create a vehicle
+            // - Update a vehicle by ID
+            // - Delete a vehicle by ID
+        // Exit
+
+        // Create a Vehicles DAO -> Data Access Object / Specific Data manager
+        // - getVehicleByID (int id)
+        // - List<Vehicles> getAllVehicles()
+        // - void createAVehicle (Vehicle vehicle)
+        // - void UpdateVehicle( int id, Vehicle vehicle)
+        // - void deleteVehicle
+}
